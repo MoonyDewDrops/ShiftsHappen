56a290ff0e0b247aaba63163a72a73b3e1bf4e8c
SheneB07 <shenebanga@gmail.com>
2026-06-09 14:21:21 +0200

    header + header include

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="header-row">
+        <div class="header-column">
+            <img src="images/ShiftHappens-logo.png" id="header-logo">
+        </div>
+        <div class="header-column">
+            <h2>Home</h2>
+            <h2>Aanbod</h2>
+            <h2>Portfolio</h2>
+            <h2>Contact</h2>
+            <h2>Webshop</h2>
+        </div>
+    </div>
+    
+</header>
